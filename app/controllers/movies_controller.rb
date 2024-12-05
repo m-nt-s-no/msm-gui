3ede5e912918f562907264318b358c6cce8c0aa7
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-05 04:01:05 +0000

    added RCAV for updating movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,4 +19,19 @@ class MoviesController < ApplicationController
     new_movie = Movie.create(title: params.fetch("title"), year: params.fetch("year"), duration: params.fetch("duration"), description: params.fetch("description"), image: params.fetch("image"), director_id: params.fetch("director_id"))
     redirect_to("/movies")
   end
+
+  def update
+    the_id = params.fetch("path_id")
+    matching_movies = Movie.where({ :id => the_id })
+    @the_movie = matching_movies.at(0)
+    @the_movie.title = params.fetch("title")
+    @the_movie.year = params.fetch("year")
+    @the_movie.duration = params.fetch("duration")
+    @the_movie.description = params.fetch("description")
+    @the_movie.image = params.fetch("image")
+    @the_movie.director_id = params.fetch("director_id")
+    @the_movie.updated_at = Time.current
+    @the_movie.save
+    render({ :template => "movie_templates/show" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/movies/create_movie", { :controller => "movies", :action =>"create"})
+  post("/movies/update_movie", { :controller => "movies", :action => "update"})
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
