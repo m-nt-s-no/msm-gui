9c79a85a657bd4817d6387ce1f2271aa6d927c7a
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-06 03:42:40 +0000

    changed deletion from post/form to get/anchor tag

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,17 @@ Rails.application.routes.draw do
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/directors/create_director", { :controller => "directors", :action =>"create"})
   post("/directors/update_director", { :controller => "directors", :action => "update"})
-  post("/delete_director/:path_id", { :controller => "directors", :action => "delete"})
+  get("/delete_director/:path_id", { :controller => "directors", :action => "delete"})
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
   post("/movies/create_movie", { :controller => "movies", :action =>"create"})
   post("/movies/update_movie", { :controller => "movies", :action => "update"})
-  post("/delete_movie/:path_id", { :controller => "movies", :action => "delete"})
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "delete"})
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
   post("/actors/create_actor", { :controller => "actors", :action =>"create"})
   post("/actors/update_actor", { :controller => "actors", :action => "update"})
-  post("/delete_actor/:path_id", { :controller => "actors", :action => "delete"})
+  get("/delete_actor/:path_id", { :controller => "actors", :action => "delete"})
 end
